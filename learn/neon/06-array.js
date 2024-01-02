71272636b53ce1e9b650a19070fd71c8b040c307
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-02 08:43:30 +0330

    add learn neon 06-array

---
+++ learn/neon/06-array.js
@@ -0,0 +1,21 @@
+let users = ["mohsen", "ali"];
+
+console.log(users);
+console.log(typeof(users));
+
+console.log(users[0]);
+console.log(users[1]);
+console.log(users[2]);
+
+users[2] = 12;
+users[3] = true;
+users[4] = "bahar";
+
+console.log(users);
+
+users[0] = "ahmad";
+users[3] = false;
+
+console.log(users);
+
+console.log(users.length);
