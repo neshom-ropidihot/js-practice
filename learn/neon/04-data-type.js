8a0c0db3d509b69407755ec8d7da2347e2e757b6
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-02 08:17:06 +0330

    add learn neon 04-data-type

---
+++ learn/neon/04-data-type.js
@@ -0,0 +1,20 @@
+let name;
+console.log(typeof(name));
+
+name = "mohsen";
+console.log(typeof(name));
+
+name = 12;
+console.log(typeof(name));
+
+name = 1.5;
+console.log(typeof(name));
+
+name = 100000000000000000000;
+console.log(typeof(name));
+
+name = null;
+console.log(typeof(name));
+
+name = false;
+console.log(typeof(name));
