00fb5e97d1b9b98071c337b2c6fd95c9bd9c000d
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-02 08:27:56 +0330

    add learn neon 05-object

---
+++ learn/neon/05-object.js
@@ -0,0 +1,22 @@
+let name = "mohsen";
+let age = 35;
+let admin = true;
+
+let person = {
+    name : "mohsen",
+    age : 35,
+    admin : true,
+}
+
+
+console.log(typeof(person));
+
+console.log(person);
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.admin);
+
+console.log(person['name']);
+console.log(person['age']);
+console.log(person['admin']);
