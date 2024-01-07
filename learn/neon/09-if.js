d812c160e33e225261aa9bc96da913de1d3895a4
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-07 07:58:01 +0330

    add learn neon 09-if

---
+++ learn/neon/09-if.js
@@ -0,0 +1,9 @@
+let time = 18;
+
+if(time >= 6 && time < 12) {
+    console.log("good morning");
+} else if(time >=12 && time < 18) {
+    console.log('good evening');
+} else {
+    console.log('good night');
+}
