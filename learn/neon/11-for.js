055a96eabcc5ab64e7a01c8889be803205204f63
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-07 08:15:14 +0330

    add learn neon 11-for

---
+++ learn/neon/11-for.js
@@ -0,0 +1,25 @@
+for(let i = 0;i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("------");
+
+for(let i = 5;i >= 1; i--) {
+    console.log(i);
+}
+
+console.log("------");
+
+for(let i = 0;i <= 10; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
+console.log("------");
+
+for(let i = 0;i <= 10; i++) {
+    if(i % 2 == 1) {
+        console.log(i);
+    }
+}
+
