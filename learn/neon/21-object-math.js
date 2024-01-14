78b1ace7133fc9f152ab943dee690b66611efc8e
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-14 08:25:31 +0330

    add learn neon 21-object-math

---
+++ learn/neon/21-object-math.js
@@ -0,0 +1,18 @@
+console.log(Math.PI);
+console.log(Math.random());
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(random(20,2));
+
+
+
+console.log(Math.round(10.9));
+console.log(Math.round(1.4));
+
+console.log('max => ', Math.max(1,6,2,3));
+console.log('min => ', Math.min(1,6,2,3));
+
+console.log(Math.abs(-2));
