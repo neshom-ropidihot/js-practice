88dbe6685a2596438ef4aa914bbddfce43db57ba
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-14 07:40:22 +0330

    add learn neon 18-object-primitive-reference

---
+++ learn/neon/18-object-primitive-reference.js
@@ -0,0 +1,21 @@
+let x = 10;
+let y = x;
+
+console.log('x => ', x);
+console.log('y => ', y);
+
+x = 20;
+
+console.log('x => ', x);
+console.log('y => ', y);
+
+let z = {value:10};
+let w = z;
+
+console.log('z => ', z);
+console.log('w => ', w);
+
+z.value = 20;
+
+console.log('z => ', z);
+console.log('w => ', w);
