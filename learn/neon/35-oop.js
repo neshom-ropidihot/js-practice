80b7245123b0d232dbc7f0ff9362f3db41dbb90f
Mohsen Tohidipour <tohidipor@gmail.com>
2024-05-05 09:44:24 +0330

    add learn neon 35-oop

---
+++ learn/neon/35-oop.js
@@ -0,0 +1,25 @@
+function Circle(radius) {
+    this.radius = radius;
+    // this.defaultLocation = {
+    //     x:0,
+    //     y:0
+    // };
+    let defaultLocation = {
+        x:0,
+        y:0
+    };
+    
+    // this.compute = function() {
+    //     // ...
+    // }
+    let compute = function() {
+        // ...
+    }
+    this.draw = function() {
+        compute();
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(10);0
+console.log(circle);
