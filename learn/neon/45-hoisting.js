e3b49a3d6d5c5173a3b9275448ba3df8e91c1444
Mohsen Tohidipour <tohidipor@gmail.com>
2024-05-15 08:32:01 +0330

    add learn neon 45-hoisting

---
+++ learn/neon/45-hoisting.js
@@ -0,0 +1,17 @@
+
+
+// const c = new Circle(); // Cannot access 'Circle' before initialization
+const s = new Square();  // Cannot access 'sq' before initialization
+
+// class declaration
+
+class Circle {
+
+}
+
+
+// class expression
+
+const Square = class {
+
+}
